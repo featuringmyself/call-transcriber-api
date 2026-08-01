314f3e7ef08a6c242e9cd244537380cdad70767b
Rudra <rudralocking@gmail.com>
2026-08-01 22:32:04 +0530

    feat:converted to a fastapi app

--- mp4_to_mp3.py
+++ mp4_to_mp3.py
@@ -3,7 +3,7 @@ from moviepy import AudioFileClip
 
 def mp4_to_mp3(video_path: str, audio_path: str) -> None:
     with AudioFileClip(video_path) as audio:
-        audio.write_audiofile(audio_path, bitrate="320k")
+        audio.write_audiofile(audio_path, bitrate="640k")
 
 
 if __name__ == "__main__":
